2fa82d6fb71244c63873137e1c89ee4bdbefae1c
Matthieu Petelot <matthieu.petelot5@etu.univ-lorraine.fr>
2026-06-01 11:41:55 +0200

    ajout de la classe serviceServeur et modif interface serviceNoeud

--- ServiceNoeudCalcul.java
+++ ServiceNoeudCalcul.java
@@ -1,4 +1,8 @@
+import java.rmi.Remote;
+import raytracer.Image;
 
-public interface ServiceNoeudCalcul {
+public interface ServiceNoeudCalcul extends Remote {
+
+    public Image calculer(int x0, int y0, int largeur, int hauteur);
 
 }
--- ServiceServeur.java
+++ ServiceServeur.java
@@ -1,10 +1,12 @@
 
+import java.rmi.Remote;
+import java.rmi.RemoteException;
 import java.util.List;
 
-public interface ServiceServeur {
+public interface ServiceServeur extends Remote {
 
-    public boolean enregistrerNoeudCalcul(ServiceNoeudCalcul serv);
+    public boolean enregistrerNoeudCalcul(ServiceNoeudCalcul serv) throws RemoteException;
 
-    public List<ServiceNoeudCalcul> getAllNoeuds();
+    public List<ServiceNoeudCalcul> getAllNoeuds() throws RemoteException;
 
 }
