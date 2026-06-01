88f94d1e9e92ce6df7fc82077151fc8f89ecfad6
PlumeDeRenard54 <tom.aubert54@outlook.fr>
2026-06-01 14:56:57 +0200

    bash compile script

--- src/Noeud/ServiceNoeudCalcul.java
+++ src/Noeud/ServiceNoeudCalcul.java
@@ -1,11 +1,12 @@
 package src.Noeud;
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 import src.raytracer.Image;
 import src.raytracer.Scene;
 
 public interface ServiceNoeudCalcul extends Remote {
 
-    public Image calculer(Scene s,int x0, int y0, int largeur, int hauteur);
+    public Image calculer(Scene s,int x0, int y0, int largeur, int hauteur) throws RemoteException;
 
 }
